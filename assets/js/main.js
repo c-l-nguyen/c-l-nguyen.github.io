2191c57931785c4b1bb23840eb31e6adbde76b95
c-l-nguyen <c.l.nguyen@icloud.com>
2020-06-10 21:39:30 -0500

    ability to click off navbar menu in mobile and social media tag change

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,13 @@ $(document).on('click', '.mobile-nav-toggle', function(e) {
   $('.mobile-nav-toggle i');
 });
   
+// Click outside toggle button to collapse 
+$(document).click(function(e) {
+  var container = $(".mobile-nav-toggle");
+  if (!container.is(e.target) && container.has(e.target).length === 0) {
+    if ($('body').hasClass('mobile-nav-active')) {
+      $('body').removeClass('mobile-nav-active');
+      $('.mobile-nav-toggle i');
+    }
+  }
+});
